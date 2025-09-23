154ef026f9834a79a1759b29a31ad559d04c7522
Divya Singh <singhdvya06@gmail.com>
2025-09-23 10:25:55 +0530

    3rd Class

---
+++ day_5/index.js
@@ -0,0 +1,51 @@
+//  console.log("Hey,,,");
+//  document.writeln("Heyy ....");
+//  console.error("error message");
+
+//  console.log(typeof a);
+ //let var and const
+ //1. redecleralation
+ //2.reassignment
+ //3.scope
+//  var a = 20;
+//  let b = 2;
+//  const pi = 3.14;
+//  console.log("a: BR "+a);
+//  console.log("b: BR "+b);
+//  console.log("pi: BR "+pi);
+//  var a = 10;
+//  console.log("a: AR "+a);
+//  let b = 34;
+//reassignment
+// var a =21;
+// const b = 22;
+// let d = 9;
+// console.log("a IN:"+a);
+// console.log("b IN "+b);
+// console.log("d IN"+d);
+// // a =0;
+// // // b = 34;
+// // d =90;
+// console.log("a AR:"+a);
+// console.log("b AR "+b);
+// console.log("d AR "+d);
+// function f(){
+//     let a = 34;
+//     var d = 90;
+//    console.log("a BR:"+a);
+//    console.log("b BR "+b);
+//    console.log("d BR "+d); 
+// }
+// f();
+// console.log("a After function R:"+a);
+//    console.log("b After function R "+b);
+//    console.log("d After function R "+d); 
+
+function f(){
+     var a=90;
+     var b = 98;
+    console.log( a);
+    console.log(b);
+}
+f();
+ 
